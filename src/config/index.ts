d0640e26ffe28fc95cbfb77cc0d7d0feb29756a9
meretamal <meretamal@uc.cl>
2022-02-06 14:12:30 -0300

    feat(database): add database connection

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const config = {
   port: parseInt(process.env.PORT || '3000', 10),
+  databaseUrl: process.env.DATABASE_URL!,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
+/* eslint-disable no-console */
+import { connect } from 'mongoose';
 import { config } from '@/config';
 import { app } from './app';
 
-app.listen(config.port, () =>
-  // eslint-disable-next-line no-console
-  console.log(`App listening on port ${config.port}`),
-);
+app.listen(config.port, async () => {
+  console.log(`App listening on port ${config.port}`);
+  try {
+    await connect(config.databaseUrl);
+    console.log('Connection to database established successfully');
+  } catch (error) {
+    console.log('Unable to connect to database');
+  }
+});
