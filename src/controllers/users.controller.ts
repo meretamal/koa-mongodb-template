bebea3bde21ed15206bdc7f201197ec12fd05e01
meretamal <meretamal@uc.cl>
2022-03-16 09:46:15 -0300

    feat(users): validate used emails when creating a user

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -6,14 +6,21 @@ export class UsersController {
   static async create(ctx: RouterContext) {
     const data = <CreateUserDto>ctx.request.body;
     const existingUser = await User.findOne({ email: data.email }).exec();
-    if (existingUser) ctx.throw(409);
-    const user = new User(data);
-    try {
-      await user.save();
-      ctx.body = user;
-      ctx.status = 201;
-    } catch (error) {
-      ctx.throw(422);
+    if (existingUser) {
+      ctx.status = 409;
+      ctx.body = {
+        message: 'Conflict',
+        errors: [`user with email ${data.email} already exists`],
+      };
+    } else {
+      const user = new User(data);
+      try {
+        await user.save();
+        ctx.status = 201;
+        ctx.body = user;
+      } catch (error) {
+        ctx.throw(422);
+      }
     }
   }
 }
