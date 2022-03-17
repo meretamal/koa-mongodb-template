c84ef094abe5529bf78b671020941ec2fc031766
meretamal <meretamal@uc.cl>
2022-03-17 10:42:02 -0300

    feat: add logging to requests

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import Koa from 'koa';
 import koaBody from 'koa-body';
+import koaLogger from 'koa-logger';
 import { router } from './routers';
 
 export const app = new Koa();
 
 app.use(koaBody());
+app.use(koaLogger());
 
 app.use(router.routes());
