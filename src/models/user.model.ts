45882a86354e4c6821648d99dd1e145c1e47adf9
meretamal <meretamal@uc.cl>
2022-03-17 12:10:47 -0300

    feat(users): hash user password when created

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,8 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, CallbackError } from 'mongoose';
+import bcrypt from 'bcrypt';
 import { IUser } from '@/interfaces/entities/user.entity';
+
+const SALT_WORK_FACTOR = 10;
 
 const schema = new Schema<IUser>({
   name: { type: String, required: true },
@@ -7,5 +10,20 @@ const schema = new Schema<IUser>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+schema.pre(
+  'save',
+  async function hashPassword(
+    this: IUser,
+    next: (err?: CallbackError | undefined) => void,
+  ) {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    const hash = await bcrypt.hash(this.password, SALT_WORK_FACTOR);
+    this.password = hash;
+    return next();
+  },
+);
 
 export const User = model<IUser>('User', schema);
