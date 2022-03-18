e69089bec1d6a38a34d5ff3f71cef7fa2b63dd29
meretamal <meretamal@uc.cl>
2022-03-18 08:54:48 -0300

    feat(auth): add expiration to jwt if needed

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,4 +7,5 @@ export const config = {
   port: parseInt(process.env.PORT || '3000', 10),
   databaseUrl: process.env.DATABASE_URL!,
   jwtSecret: process.env.JWT_SECRET!,
+  jwtExpiration: process.env.JWT_EXPIRATION || undefined,
 };
--- src/utils/jwt/generate-token.ts
+++ src/utils/jwt/generate-token.ts
@@ -1,10 +1,14 @@
-import { sign } from 'jsonwebtoken';
+import { sign, SignOptions } from 'jsonwebtoken';
 import { config } from '@/config';
 import { IUser } from '@/interfaces/entities/user.entity';
 
 export function generateToken(user: IUser) {
   return new Promise((resolve, reject) => {
-    sign({ sub: user.id }, config.jwtSecret, {}, (err, token) =>
+    const options: SignOptions = {};
+    if (config.jwtExpiration) {
+      options.expiresIn = config.jwtExpiration;
+    }
+    sign({ sub: user.id }, config.jwtSecret, options, (err, token) =>
       err ? reject(err) : resolve(token),
     );
   });
