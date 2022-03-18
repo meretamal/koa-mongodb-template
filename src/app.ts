7e9d0949cc42dcda513435046857ef8bed5fb3a6
meretamal <meretamal@uc.cl>
2022-03-17 22:15:59 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import Koa from 'koa';
 import koaBody from 'koa-body';
+import koaCors from '@koa/cors';
 import koaLogger from 'koa-logger';
 import { router } from './routers';
 
 export const app = new Koa();
 
 app.use(koaBody());
+app.use(koaCors());
 app.use(koaLogger());
 
 app.use(router.routes());
