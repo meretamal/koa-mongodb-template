44316d920ced5e083989adf4766df7a1c28b01e3
meretamal <meretamal@uc.cl>
2022-03-18 09:27:23 -0300

    feat: manage not allowed methods

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,37 @@
-import Koa from 'koa';
+import Koa, { Next } from 'koa';
 import koaBody from 'koa-body';
 import koaCors from '@koa/cors';
 import koaLogger from 'koa-logger';
+import { RouterContext } from '@koa/router';
+import { isHttpError } from '@/utils/errors/is-http-error';
 import { router } from './routers';
 
 export const app = new Koa();
+
+app.use(async (ctx: RouterContext, next: Next) => {
+  try {
+    await next();
+  } catch (error) {
+    if (isHttpError(error)) {
+      ctx.status = error.statusCode || error.status;
+      const { message, errors } = error;
+      ctx.body = {
+        message,
+        errors,
+      };
+    } else {
+      ctx.status = 500;
+      ctx.body = {
+        message: 'Internal Server Error',
+      };
+      console.log(error);
+    }
+  }
+});
 
 app.use(koaBody());
 app.use(koaCors());
 app.use(koaLogger());
 
 app.use(router.routes());
+app.use(router.allowedMethods({ throw: true }));
