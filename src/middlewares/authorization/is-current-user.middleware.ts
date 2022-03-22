d1f8e66f7cc4efa2b5e3060964e0d94240b9add2
meretamal <meretamal@uc.cl>
2022-03-22 11:05:38 -0300

    refactor: remove unnecessary console log statements

--- src/middlewares/authorization/is-current-user.middleware.ts
+++ src/middlewares/authorization/is-current-user.middleware.ts
@@ -3,8 +3,6 @@ import { RouterContext } from '@koa/router';
 
 export async function isCurrentUserMiddleware(ctx: RouterContext, next: Next) {
   const { authData, user } = ctx.state;
-  console.log(authData.sub);
-  console.log(user.id);
   if (authData.sub === user.id) {
     await next();
   } else {
