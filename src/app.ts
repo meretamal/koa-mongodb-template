bcf501b13fa1b7e0a78d15d6c1d3aa93713bf4a5
meretamal <meretamal@uc.cl>
2022-04-08 10:18:23 -0400

    style: ignore eslint console log offence

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ app.use(async (ctx: RouterContext, next: Next) => {
       ctx.body = {
         message: 'Internal Server Error',
       };
+      // eslint-disable-next-line no-console
       console.log(error);
     }
   }
