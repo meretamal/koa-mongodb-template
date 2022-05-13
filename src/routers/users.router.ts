640a725a083f6393ff13510654bc7a78df629c0e
meretamal <meretamal@uc.cl>
2022-05-13 16:58:33 -0400

    refactor: move user middlewares to the users feature

--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -3,7 +3,7 @@ import { object, string } from 'yup';
 import { UsersController } from '@/features/users/users.controller';
 import { vaidateRequestBodyMiddleware } from '@/middlewares/validation/validate-request-body.middleware';
 import { vaidateRequestParamsMiddleware } from '@/middlewares/validation/validate-request-params.middleware';
-import { findUserByIdMiddleware } from '@/middlewares/users/find-user-by-id.middleware';
+import { findUserByIdMiddleware } from '@/features/users/middlewares/find-user-by-id.middleware';
 import { isCurrentUserMiddleware } from '@/middlewares/authorization/is-current-user.middleware';
 import { IUpdateUserDto } from '@/interfaces/dtos/users/update-user.dto';
 import { objectId } from '@/utils/yup/custom-schemas/object-id.schema';
