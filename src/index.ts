2245fba105dc5a1aa7749305e87d063431e7a737
meretamal <meretamal@uc.cl>
2022-05-26 23:48:34 -0400

    refactor: remove unused code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 /* eslint-disable no-console */
-import { connect } from 'mongoose';
 import { environment } from '@/config/environment';
 import { app } from './app';
 
 app.listen(environment.port, async () => {
   console.log(`App listening on port ${environment.port}`);
-  try {
-    await connect(environment.database.url);
-    console.log('Connection to database established successfully');
-  } catch (error) {
-    console.log('Unable to connect to database');
-  }
 });
--- src/interfaces/entities/user.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface IUser extends Document {
-  name: string;
-  lastName: string;
-  email: string;
-  password: string;
-  comparePassword(password: string): Promise<boolean>;
-}
