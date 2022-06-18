b83fbca8e40f2cd33f439a8791214b6d77c678aa
meretamal <meretamal@uc.cl>
2022-06-17 21:50:47 -0400

    test(users): add a user factory

---
+++ test/factories/user.factory.ts
@@ -0,0 +1,11 @@
+import { Prisma } from '@prisma/client';
+import { Factory } from 'fishery';
+import { faker } from '@faker-js/faker';
+
+export const userFactory = Factory.define<Prisma.UserCreateInput>(() => ({
+  id: faker.datatype.uuid(),
+  email: faker.internet.email(),
+  name: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  password: faker.internet.password(),
+}));
