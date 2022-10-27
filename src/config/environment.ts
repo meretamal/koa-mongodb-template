6ececf6a251451b1933eb365854ca6d115545aa0
meretamal <meretamal@uc.cl>
2022-10-27 13:13:53 -0300

    refactor: remove mailer and redis env variables

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -15,17 +15,4 @@ export const environment = {
     secret: process.env.JWT_SECRET!,
     expiration: process.env.JWT_EXPIRATION,
   },
-  mailer: {
-    smtpHost: process.env.MAILER_SMTP_HOST!,
-    smtpPort: Number(process.env.MAILER_SMTP_PORT || 465),
-    smtpUsername: process.env.MAILER_SMTP_USERNAME!,
-    smtpPassword: process.env.MAILER_SMTP_PASSWORD!,
-    sender: process.env.MAILER_SENDER!,
-  },
-  redis: {
-    host: process.env.REDIS_HOST || 'localhost',
-    port: Number(process.env.REDIS_PORT || 6379),
-    password: process.env.REDIS_PASSWORD!,
-    url: process.env.REDIS_URL!,
-  },
 };
