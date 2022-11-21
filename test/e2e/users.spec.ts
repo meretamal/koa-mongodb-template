5b93ca91e741cdf6b9fff41b0cb4b555455aed46
meretamal <meretamal@uc.cl>
2022-11-20 23:52:33 -0300

    test(users): test the get users detail endpoint

--- test/e2e/users.spec.ts
+++ test/e2e/users.spec.ts
@@ -1,4 +1,5 @@
 import request from 'supertest';
+import { ObjectID } from 'bson';
 import { app } from '@/app';
 import { prisma } from '@/prisma/client.prisma';
 import { userFactory } from '../factories/user.factory';
@@ -29,6 +30,49 @@ describe('Users', () => {
       const response = await request(app.callback()).get('/users');
       expect(response.status).toBe(401);
       expect(response.body.message).toBe(AUTHENTICATION_ERROR);
+    });
+  });
+
+  describe('GET /users/:id', () => {
+    it('should return a successful response', async () => {
+      const { user, token } = await generateTestingToken();
+      const response = await request(app.callback())
+        .get(`/users/${user.id}`)
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(user);
+    });
+
+    it('should return an unauthorized response', async () => {
+      const response = await request(app.callback()).get('/users/1');
+      expect(response.status).toBe(401);
+      expect(response.body.message).toBe(AUTHENTICATION_ERROR);
+    });
+
+    it('should return a not found response', async () => {
+      const objectId = new ObjectID();
+      const { token } = await generateTestingToken();
+      const response = await request(app.callback())
+        .get(`/users/${objectId}`)
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body.errors).toContain(
+        `user with ObjectId ${objectId} does not exist`,
+      );
+    });
+
+    it('should return a bad request response', async () => {
+      const { token } = await generateTestingToken();
+      const response = await request(app.callback())
+        .get('/users/not-valid-object-id')
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors).toContain(
+        'not-valid-object-id must be a valid ObjectId',
+      );
     });
   });
 });
